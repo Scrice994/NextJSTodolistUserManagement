ed6bf98745525208002042e496b030b2058f482a
Scrice <davidemassimo94@gmail.com>
2023-11-03 11:04:03 +0100

    implemented signupAPI

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -5,6 +5,8 @@ import UserModel from "../models/mongo/userSchema";
 import { UserRepository } from "../repositories/UserRepository";
 import { UserEntity } from "../models/UserEntity";
 import { UserCRUD } from "../CRUD/UserCRUD";
+import bcrypt from "bcrypt";
+import createHttpError from "http-errors";
 
 const USER_DATA_STORAGE = new MongoDataStorage<UserEntity>(UserModel);
 const USER_REPOSITORY = new UserRepository(USER_DATA_STORAGE);
@@ -23,7 +25,29 @@ export const signup: RequestHandler<unknown, unknown, SignUpBody, unknown> =  as
     const { username, email, password: passwordRaw, tenantId } = req.body;
 
     try {
-        res.status(200).json("new User");
+        const findExistingUsername = await USER_CRUD.readOne({ username });
+        if(findExistingUsername){
+            throw createHttpError(409, "Username already taken");
+        }
+
+        const findExistingEmail = await USER_CRUD.readOne({ email });
+        if(findExistingEmail){
+            throw createHttpError(409, "A user with this email address already exists. Please log in instead");
+        }
+
+        const passwordHashed = await bcrypt.hash(passwordRaw, 10); 
+
+        const newUser = await USER_CRUD.create({
+            username,
+            email,
+            password: passwordHashed,
+            tenantId,
+            userRole: "Admin"
+        });
+
+        delete newUser.password;
+
+        res.status(200).json(newUser);
     } catch (error) {
         next(error);
     }
--- tests/api/usersAPI.test.ts
+++ tests/api/usersAPI.test.ts
@@ -1,52 +1,71 @@
 import axios from "axios";
+import { clearDB } from "./mongoTestUtils";
+import { UserCRUD } from "../../src/CRUD/UserCRUD";
+import { MongoDataStorage } from "../../src/dataStorage/MongoDataStorage";
+import { UserEntity } from "../../src/models/UserEntity";
+import { UserRepository } from "../../src/repositories/UserRepository";
+import UserModel from "../../src/models/mongo/userSchema";
 
 describe("unit", () => {
     describe("userAPI", () => {
+
+        beforeEach( async () => {
+            await clearDB();
+        });
+
+        const USER_DATA_STORAGE = new MongoDataStorage<UserEntity>(UserModel);
+        const USER_REPOSITORY = new UserRepository(USER_DATA_STORAGE);
+        const USER_CRUD = new UserCRUD(USER_REPOSITORY);
+
+        const userAPIBaseUrl = "http://localhost:4000";
+        const testUser = {
+            username: "testUsername",
+            password: "testPassword",
+            email: "testEmail@gmail.com"
+        }
+
         describe("SignUp", () => {
             it("Should return new saved user without password and email", async () => {
-                const createNewUser = await axios.post("http://localhost:4000/signup", {
-                    username: "testUsername",
-                    password: "testPassword",
-                    email: "testEmail@gmail.com"
-                });
+                const createNewUser = await axios.post(userAPIBaseUrl + "/signup", testUser);
+                const { email, createdAt, updatedAt, ...createNewresult } = createNewUser.data;
 
-                const findUser = await axios.get("http://localhost:4000/find-users");
+                const findUser = await USER_CRUD.readOne({});
+                const { createdAt: findCreatedAt, updatedAt: findUpdatedAt, ...findUserResult } = findUser;
 
-                expect(createNewUser).toEqual([findUser]);
+                expect(createNewUser.status).toBe(200);
+                expect(findUserResult).toEqual(createNewresult);
             });
 
             it("Should return error when username is already taken", async () => {
-                const createNewUser = await axios.post("http://localhost:4000/signup", {
-                    username: "testUsername",
-                    password: "testPassword",
-                    email: "testEmail@gmail.com"
+                await axios.post(userAPIBaseUrl + "/signup", testUser);
+                const createUserWithSameUsername = await axios.post("http://localhost:4000/signup", {...testUser, email: "testEmail2@gmail.com" })
+                .catch( err => {
+                    expect(err.response.status).toBe(409);
+                    expect(err.response.data).toEqual({ error: "Username already taken" });
                 });
 
-                const createUserWithSameUsername = await axios.post("http://localhost:4000/signup", {
-                    username: "testUsername",
-                    password: "testPassword2",
-                    email: "testEmail2@gmail.com"
-                });
-
-                expect(createUserWithSameUsername.status).toBe(409);
-                expect(createUserWithSameUsername.data).toEqual({error: "Username already taken"});
+                expect(createUserWithSameUsername).toBe(undefined);
             });
 
             it("Should return error when email is already taken", async () => {
-                const createNewUser = await axios.post("http://localhost:4000/signup", {
-                    username: "testUsername",
-                    password: "testPassword",
-                    email: "testEmail@gmail.com"
+                await axios.post("http://localhost:4000/signup", testUser);
+                const createUserWithSameEmail = await axios.post("http://localhost:4000/signup", {...testUser, username: "testUsername2" })
+                .catch( err => {
+                    expect(err.response.status).toBe(409);
+                    expect(err.response.data).toEqual({ error: "A user with this email address already exists. Please log in instead" });
                 });
 
-                const createUserWithSameUsername = await axios.post("http://localhost:4000/signup", {
-                    username: "testUsername2",
-                    password: "testPassword2",
-                    email: "testEmail@gmail.com"
+                expect(createUserWithSameEmail).toBe(undefined);
+            });
+
+            it("Should return error when an uknown error occour in the server(ErrorHandler-test)", async () => {
+                const createNewUser = await axios.post("http://localhost:4000/signup")
+                .catch(err => {
+                    expect(err.response.status).toBe(500);
+                    expect(err.response.data).toEqual({ error: "An unknown error occour" });
                 });
 
-                expect(createUserWithSameUsername.status).toBe(409);
-                expect(createUserWithSameUsername.data).toEqual({error: "A user with this email address already exists. Please log in instead"});
+                expect(createNewUser).toBe(undefined);
             });
         });
     });
